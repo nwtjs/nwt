5b728c9d392f0d950a0eb355c63b6076c5982b99
Kevin Grandon <kevingrandon@yahoo.com>
2012-02-11 19:04:14 -0800

    Add cubic-bezier css anim test.

--- src/anim/test/anim.test.js
+++ src/anim/test/anim.test.js
@@ -5,7 +5,8 @@ nwt.unit
         '<div id="anim2" style="position:absolute;top:20px;left:0px;">ease</div>',
         '<div id="anim3" style="position:absolute;top:40px;left:0px;">ease-in</div>',
         '<div id="anim4" style="position:absolute;top:60px;left:0px;">ease-out</div>',
-        '<div id="anim5" style="position:absolute;top:80px;left:0px;">ease-out-in</div>'
+        '<div id="anim5" style="position:absolute;top:80px;left:0px;">ease-out-in</div>',
+        '<div id="anim6" style="position:absolute;top:100px;left:0px;">cubic-bezier</div>'
         ].join(''))
 .equal(
 	function () {
@@ -14,6 +15,7 @@ nwt.unit
 		nwt.one('#anim3').anim({left:400}, 2, 'ease-in').wait(4).anim({left:0}, 2, 'ease-in');
 		nwt.one('#anim4').anim({left:400}, 2, 'ease-out').wait(4).anim({left:0}, 2, 'ease-out');
 		nwt.one('#anim5').anim({left:400}, 2, 'ease-in-out').wait(4).anim({left:0}, 2, 'ease-in-out');
+		nwt.one('#anim6').anim({left:400}, 2, 'cubic-bezier(1,0,1,0)').wait(4).anim({left:0}, 2, 'cubic-bezier(1,0,1,0)');
 
 		return true;
 	},
