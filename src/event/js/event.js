d896b27fa4365ee944ffa882297a9e5cbb97ae22
Kevin Grandon <kevingrandon@yahoo.com>
2012-02-13 11:54:52 -0800

    Quick and dirty domready method.

--- src/event/js/event.js
+++ src/event/js/event.js
@@ -76,11 +76,71 @@ on: function (implementOn, event, callback, context, once) {
  */
 off: function (implementOn, event, callback) {
 	implement.on.removeEventListener(event, callback);
+},
+
+/**
+ * Fires a callback when the dom is ready
+ */
+ready: function(fn) {
+	/*!
+	 * contentloaded.js
+	 *
+	 * Author: Diego Perini (diego.perini at gmail.com)
+	 * Summary: cross-browser wrapper for DOMContentLoaded
+	 * Updated: 20101020
+	 * License: MIT
+	 * Version: 1.2
+	 *
+	 * URL:
+	 * http://javascript.nwbox.com/ContentLoaded/
+	 * http://javascript.nwbox.com/ContentLoaded/MIT-LICENSE
+	 *
+	 */
+	
+	// @win window reference
+	// @fn function reference
+
+	var done = false,
+		top = true,
+
+		win = window,
+
+		doc = win.document, root = doc.documentElement,
+	
+		add = doc.addEventListener ? 'addEventListener' : 'attachEvent',
+		rem = doc.addEventListener ? 'removeEventListener' : 'detachEvent',
+		pre = doc.addEventListener ? '' : 'on',
+	
+		init = function(e) {
+			if (e.type == 'readystatechange' && doc.readyState != 'complete') return;
+			(e.type == 'load' ? win : doc)[rem](pre + e.type, init, false);
+			if (!done && (done = true)) fn.call(win, e.type || e);
+		},
+	
+		poll = function() {
+			try { root.doScroll('left'); } catch(e) { setTimeout(poll, 50); return; }
+			init('poll');
+		};
+
+	if (doc.readyState == 'complete') fn.call(win, 'lazy');
+	else {
+		if (doc.createEventObject && root.doScroll) {
+			try { top = !win.frameElement; } catch(e) { }
+			if (top) poll();
+		}
+		doc[add](pre + 'DOMContentLoaded', init, false);
+		doc[add](pre + 'readystatechange', init, false);
+		win[add](pre + 'load', init, false);
+	}
 }
+
 };
 
 
 nwt.event = new NWTEvent();
+
+nwt.ready = nwt.event.ready;
+
 
 /**
  * Stub out the Node addEventListener/removeEventListener interfaces
