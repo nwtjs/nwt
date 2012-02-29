d55ede3a3131b1557665ad78b765d61741bdcdd5
Kevin Grandon <kevingrandon@yahoo.com>
2012-02-29 06:17:03 +0000

    Animations for collapsible groups.

--- plugins/bootstrap/collapse.js
+++ plugins/bootstrap/collapse.js
@@ -12,6 +12,11 @@ nwt.register({
 			var collapseGroup = el.ancestor('.accordion-group'),
 				body = collapseGroup.one('.accordion-body');
 
+			// Add the in class for transitions
+			if (!body.hasClass('in')) {
+				body.addClass('in');
+			}
+
 			// If we are expanded, collapse and return
 			if (parseInt(body.getStyle('height'), 10) !== 0) {
 				body.setStyle('height', '0px');
@@ -21,7 +26,7 @@ nwt.register({
 			// Shrink all others in the accordian
 			body.ancestor('.accordion').all('.collapse').setStyle('height', '0px');
 
-			body.setStyle('height', 'auto');
+			body.setStyle('height', body.get('scrollHeight'));
 		}
 	}
 });
