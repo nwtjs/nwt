4203b8707b47b7d44b4b1e0938c2857258d254fe
Kevin Grandon <kevingrandon@yahoo.com>
2012-02-29 06:00:35 +0000

    Super basic carousel.

---
+++ plugins/bootstrap/carousel.js
@@ -0,0 +1,69 @@
+nwt.register({
+
+	name: 'Carousel',
+
+	methods: {
+		init: function () {
+			nwt.event.live('data-slide', /(.*)/, this.slide);
+		},
+
+
+		/**
+		 * Sets the carousel to autoplay
+		 */
+		play: function() {
+
+			  this.interval = setInterval(this.next, 5000);
+
+			  return this;
+		},
+
+
+		/**
+		 * Pauses the carousel
+		 */
+		pause: function () {
+			clearInterval(this.interval)
+			this.interval = null
+			return this
+		},
+
+		/**
+		 * Rotates to the next item in the carousel
+		 */
+		next: function() {
+		},
+
+
+		/**
+		 * Slides the carousel
+		 * Dispatched callback from nwt.event.live
+		 */
+		slide: function(el, action) {
+			var dir = action[1],
+
+				carouselEl = el.ancestor('.carousel'),
+
+				method = dir == 'next' ? 'next' : 'previous',
+
+				direction = dir == 'next' ? 'left' : 'right',
+
+				activeItem = carouselEl.one('.active'),
+
+				nextItem = activeItem[method]();
+
+			if (nextItem && nextItem._node) {
+				nextItem.addClass(dir)
+				nextItem.region() // force reflow
+				activeItem.addClass(direction)
+				nextItem.addClass(direction)
+
+				setTimeout(function(){
+					nextItem.removeClass(direction).removeClass(dir).addClass('active')
+					activeItem.removeClass('active').removeClass(direction)
+				}, 600)
+			}
+		}
+	}
+});
+nwt.plugin('Carousel');
