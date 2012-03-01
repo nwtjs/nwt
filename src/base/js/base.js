3fadbc7b0ac65562a99dff365ef54617787c9c0f
Kevin Grandon <kevingrandon@yahoo.com>
2012-02-29 20:41:37 -0800

    DelayableInterface work.

--- src/base/js/base.js
+++ src/base/js/base.js
@@ -7,51 +7,87 @@ function NWT() {
 };
 
 NWT.prototype = {
+
 /**
- * Lazy implementation method
+ * Implements an interface on an object
  */
 implement: function(implClass, modClass) {
 
 	var impls = {
 		DelayableQueue : [
+			/**
+			 * Returns a queueable interface to the original object
+			 * This allows us to insert delays between chainable method calls
+			 * Methods outside the scope of the chain are not delayed
+			 * E.g. if a method performs setTimeout, followed by a wait, 
+			 * methods in the setTimeout call are not delayed.
+			 */
 			'wait', function () {
 
-				var delay = 0;
+				/**
+				 * Queueable object which implements methods from another interface
+				 * @param object Context for the callback
+				 */
+				function QueueableObject(context) {
+					this.queue = [];
+					this.context = context;
+				}
+				
+				QueueableObject.prototype = {
+					_add: function(func, args) {
 
+						var self = this;
+
+						self.queue.push({type: 'chain', func: func, args: args});
+						return self._process();
+					},
+
+					/**
+					 * Process the queue
+					 * Shifts an item off the queue and waits for it to finish
+					 */
+					_process: function() {
+						var self = this, item;
+
+						if (!self.queue.length) {
+							return;
+						}
+						
+						item = self.queue.shift();
+
+						if (item.type == 'wait') {
+							setTimeout(function(){
+								self._process();
+							}, item.duration*1000);
+						} else {
+							self.context = item.func.apply(self.context, item.args);
+							self._process();
+						}
+						return self;
+					},
+
+					/**
+					 * Updates the current delay timer
+					 */
+					wait: function(duration) {
+						this.queue.push({type: 'wait', duration: duration});
+						return this;
+					}
+				};
+				
 				return function (duration) {
 
-					delay += (duration*1000);
-
 					var self = this,
-
-					funcCache = {},
+						mockObj = new QueueableObject(self);
 
 					/**
 					 * Returns an executable function
 					 * uses setTimeout and the total queueTimer
 					 */
 					getQueuedFunction = function(func) {
-
-						funcCache[func] = self[func];
-
 						return function() {
-							var args = [],
-								i,
-								argLen = arguments.length;
-
-							for (i = 0; i < argLen ; i++ ) {
-								args.push(arguments[i]);
-							}
-
-							if( delay <= 0 ) {
-								return funcCache[func].apply(self, args);
-							}
-
-							setTimeout(function() {
-								delay -= (duration*1000);
-								funcCache[func].apply(self, args);
-							}, delay);
-							return self;
+							mockObj._add(func, arguments);
+							return mockObj;
 						}
 					};
 
@@ -60,11 +96,13 @@ implement: function(implClass, modClass) {
 					 * We can unwrap at the end if the current wait is 0
 					 */
 					for( var i in self ) {
-						if (typeof self[i] != 'function' || i == 'wait' || funcCache[i] ) { continue; }
-						self[i] = getQueuedFunction(i);
+						if (typeof self[i] != 'function' || i == 'wait' ) { continue; }
+						mockObj[i] = getQueuedFunction(self[i]);
 					}
 
-					return self;
+					mockObj.wait(duration);
+					
+					return mockObj;
 				}
 			}
 		]
